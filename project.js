c94f3d64a294685bad16c53187c8155b47b95680
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-08-17 20:37:08 +0600

    added amount

--- project.js
+++ project.js
@@ -1,10 +1,28 @@
-let total =0;
-function cardTotal(target){
-    const targetBtn = target.parentNode.childNodes[5].innerText.split(' ')[2];
-    total = parseFloat(total) + parseFloat(targetBtn);
+let total = 0;
+let count = 0;
+function cardTotal(target) {
+    const productTitle = target.parentNode.childNodes[1].innerText;
+    const li = document.createElement('h6');
+    const codeHeroContainer = document.getElementById('code-hero-container');
+    // const cont =codeHeroContainer.appendChild(li);
+    const llll = codeHeroContainer.children;
+    codeHeroContainer.appendChild(li);
+    li.innerText = productTitle;
+
+
+
+    
+    const productPrice = target.parentNode.childNodes[5].innerText.split(' ')[2];
+    total = parseFloat(total) + parseFloat(productPrice);
     const cardTotal = document.getElementById('card-total').innerText = total;
     document.getElementById('all-discount').innerText = cardTotal*0.3
     document.getElementById('grand-total').innerText = cardTotal-cardTotal*0.3;
-    
-    
+    const totalProductAtCard = count+=1;
+    count = totalProductAtCard;
+    const productCount = document.getElementById('count-item');
+    productCount.innerText= totalProductAtCard;
+    const productCount2 = document.getElementById('count-item2');
+    productCount2.innerText= totalProductAtCard;
+    const subtotal = document.getElementById('subtotal');
+    subtotal.innerText = cardTotal-cardTotal*0.3;
 }
