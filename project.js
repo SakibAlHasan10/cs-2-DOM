2a34a57038bba4f191b04f2a132a04f21c41cd7c
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-08-18 00:10:48 +0600

    card section

--- project.js
+++ project.js
@@ -4,25 +4,22 @@ function cardTotal(target) {
     const productTitle = target.parentNode.childNodes[1].innerText;
     const li = document.createElement('h6');
     const codeHeroContainer = document.getElementById('code-hero-container');
-    // const cont =codeHeroContainer.appendChild(li);
-    const llll = codeHeroContainer.children;
+    const heroChildren = codeHeroContainer.children;
     codeHeroContainer.appendChild(li);
     li.innerText = productTitle;
-
-
-
-    
     const productPrice = target.parentNode.childNodes[5].innerText.split(' ')[2];
     total = parseFloat(total) + parseFloat(productPrice);
     const cardTotal = document.getElementById('card-total').innerText = total;
-    document.getElementById('all-discount').innerText = cardTotal*0.3
-    document.getElementById('grand-total').innerText = cardTotal-cardTotal*0.3;
-    const totalProductAtCard = count+=1;
+    document.getElementById('all-discount').innerText = cardTotal * 0.3
+    document.getElementById('grand-total').innerText = cardTotal - cardTotal * 0.3;
+    const totalProductAtCard = count += 1;
     count = totalProductAtCard;
     const productCount = document.getElementById('count-item');
-    productCount.innerText= totalProductAtCard;
+    productCount.innerText = totalProductAtCard;
     const productCount2 = document.getElementById('count-item2');
-    productCount2.innerText= totalProductAtCard;
+    productCount2.innerText = totalProductAtCard;
     const subtotal = document.getElementById('subtotal');
-    subtotal.innerText = cardTotal-cardTotal*0.3;
+    subtotal.innerText = cardTotal - cardTotal * 0.3;
 }
+
+    
